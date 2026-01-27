6ba5b48be5a8dd16764fd6e7d53fc8619fe505dc
haresh742 <har742@gmail.com>
2026-01-27 14:48:40 +0530

    Add files via upload

---
+++ twonumbersinput.py
@@ -0,0 +1,18 @@
+
+
+num1 = 5
+
+num2 = 10
+
+
+
+print("Enter the first number :" ,num1)
+print("Enter the second number :" ,num2)
+result = num1 + num2
+print("The addition is :",result)
+result1 = num1 - num2
+print("The substraction is :",result1)
+result2 = num1 * num2
+print("The multiplication is :",result2)
+result3 = num1 / num2
+print("The divison is :",result3)
