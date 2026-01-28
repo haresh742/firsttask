65fca783c2f17cad2731169f56bbe071a7a1259b
haresh742 <har742@gmail.com>
2026-01-28 18:22:30 +0530

    Add files via upload

---
+++ area_of_90degree_triangle.py
@@ -0,0 +1,10 @@
+"""
+When the leanth of two sides are known - a, b
+semi perameter(s)=1/2 * a * b
+"""
+a= float(input("enter first side:"))
+b= float(input("enter second side:"))
+
+s= 1/2 * a * b
+area=( s)
+print("The area of the triangle is",round(area, 2))
